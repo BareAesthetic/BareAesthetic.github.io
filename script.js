9393f6a793178585f679c15fc6b783bbd84bbcf6
BareAesthetic <b66451051@gmail.com>
2026-03-08 13:33:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-
 // переключение страниц
 
 function openPage(id){
 
 document.querySelectorAll(".page").forEach(p=>{
-
 p.classList.remove("active")
-
 })
 
 document.getElementById(id).classList.add("active")
@@ -30,8 +27,8 @@ const texts=[
 "Лучшие визуалы",
 "Красивый HUD",
 "Плавные анимации",
-"Лучший Minecraft мод",
-"Эстетика Sakura"
+"Эстетика Sakura",
+"Лучший мод"
 
 ]
 
@@ -44,3 +41,59 @@ i=(i+1)%texts.length
 document.getElementById("changingText").textContent=texts[i]
 
 },2500)
+
+
+
+// падающие лепестки
+
+const canvas=document.getElementById("sakura")
+const ctx=canvas.getContext("2d")
+
+canvas.width=window.innerWidth
+canvas.height=window.innerHeight
+
+let petals=[]
+
+for(let i=0;i<40;i++){
+
+petals.push({
+
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+size:Math.random()*6+4,
+speed:Math.random()*1+0.5
+
+})
+
+}
+
+function draw(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height)
+
+ctx.fillStyle="pink"
+
+petals.forEach(p=>{
+
+ctx.beginPath()
+
+ctx.arc(p.x,p.y,p.size,0,Math.PI*2)
+
+ctx.fill()
+
+p.y+=p.speed
+
+if(p.y>canvas.height){
+
+p.y=0
+p.x=Math.random()*canvas.width
+
+}
+
+})
+
+requestAnimationFrame(draw)
+
+}
+
+draw()
